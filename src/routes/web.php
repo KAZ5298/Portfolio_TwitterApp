4cf0ba9e5a9ce8a200166d11afdee0270880831f
KAZUHIKO <kazuhiko.d@gmail.com>
2023-08-24 14:02:15 +0900

    ログイン後、「全てのつぶやき一覧画面」へ遷移するように

--- src/routes/web.php
+++ src/routes/web.php
@@ -24,9 +24,13 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/allTweets', function () {
+    return view('tweet.index');
+})->middleware(['auth', 'verified'])->name('allTweetGet');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
