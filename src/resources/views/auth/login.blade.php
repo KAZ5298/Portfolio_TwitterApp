780c2ffd742eaebd0793642a873a28dabd41e943
KAZ5298 <kazuhiko.d@gmail.com>
2023-10-04 21:32:03 +0900

    レスポンシブ対応　ユーザー登録・編集確認画面

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -9,6 +9,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@30,400,0,0">
 </head>
 
 <body class="bg-primary-subtle">
@@ -22,45 +24,45 @@
 
     {{-- メインコンテンツ --}}
     <div class="main">
-        <div class="container login">
+        <div class="container">
             <form method="POST" action="{{ route('login') }}">
                 @csrf
 
-                <div class="input">
-                    <label>アカウント名（ユーザーＩＤ）または、メールアドレス</label>
-                    <br>
-                    <div class="mb-4">
-                        @if (!$errors->has('email_or_id'))
-                            <input class="form" type="text" name="email_or_id">
-                            <label>　</label>
-                        @else
-                            <input style="border: solid 2px red;" class="form" type="text" name="email_or_id">
-                            @foreach ($errors->get('email_or_id') as $message)
-                                <label style="color: red">{{ $message }}</label>
-                            @endforeach
-                        @endif
-                    </div>
+                <div class="input name">
+                    <label for="name">アカウント名（ユーザーＩＤ）または、メールアドレス</label>
+                    @if (!$errors->has('email_or_id'))
+                        <input id="name" class="form" type="text" name="email_or_id">
+                    @else
+                        <input style="border: solid 2px red;" id="name" class="form" type="text"
+                            name="email_or_id">
+                        @foreach ($errors->get('email_or_id') as $message)
+                            <label style="color: red">{{ $message }}</label>
+                        @endforeach
+                    @endif
                 </div>
 
-                <div class="input">
-                    <label>パスワード</label>
-                    <br>
-                    <div class="mb-4">
-                        @if (!$errors->has('password'))
-                            <input class="form" type="password" name="password">
-                            <label>　</label>
-                        @else
-                            <input style="border: solid 2px red;" class="form" type="password" name="password">
-                            @foreach ($errors->get('password') as $message)
-                                <label style="color: red">{{ $message }}</label>
-                            @endforeach
-                        @endif
-                    </div>
+                <div class="input password">
+                    <label for="password">パスワード</label>
+                    @if (!$errors->has('password'))
+                        <input id="password" class="form" type="password" name="password">
+                    @else
+                        <input style="border: solid 2px red;" id="password" class="form" type="password"
+                            name="password">
+                        @foreach ($errors->get('password') as $message)
+                            <label style="color: red">{{ $message }}</label>
+                        @endforeach
+                    @endif
                 </div>
 
                 <div class="button">
-                    <a href="{{ route('register') }}" class="btn btn-primary">新規登録</a>
-                    <button type="submit" class="btn btn-primary">ログイン</button>
+                    <a href="{{ route('register') }}" class="btn btn-primary">
+                        <span class="material-symbols-outlined"> how_to_reg </span>
+                        <span class="btn-name">新規登録</span>
+                    </a>
+                    <button type="submit" class="btn btn-primary">
+                        <span class="material-symbols-outlined"> login </span>
+                        <span class="btn-name">ログイン</span>
+                    </button>
                 </div>
             </form>
         </div>
--- src/resources/views/profile/check.blade.php
+++ src/resources/views/profile/check.blade.php
@@ -10,6 +10,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@30,400,0,0">
 </head>
 
 <body class="bg-primary-subtle">
@@ -30,28 +32,27 @@
 
                 <table>
                     <tr>
-                        <th><label>アカウント名（ユーザーＩＤ）</label></th>
-                        <td><label>{{ $user->name }}</label></td>
+                        <th><label class="input">アカウント名（ユーザーＩＤ）</label></th>
+                        <td><label class="input">{{ $user->name }}</label></td>
                         <input type="hidden" name="name" value="{{ $user->name }}">
                     </tr>
                     <tr>
-                        <th><label>メールアドレス</label></th>
-                        <td><label>{{ $user->email }}</label></td>
+                        <th><label class="input">メールアドレス</label></th>
+                        <td><label class="input">{{ $user->email }}</label></td>
                         <input type="hidden" name="email" value="{{ $user->email }}">
                     </tr>
                     <tr>
-                        <th><label>ニックネーム（一覧画面での表示名）</label></th>
-                        <td><label>{{ $user->nickname }}</label></td>
+                        <th><label class="input">ニックネーム（一覧画面での表示名）</label></th>
+                        <td><label class="input">{{ $user->nickname }}</label></td>
                         <input type="hidden" name="nickname" value="{{ $user->nickname }}">
                     </tr>
                     <tr>
-                        <th><label>プロフィール画像</label></th>
+                        <th><label class="input">プロフィール画像</label></th>
                         <td>
                             @if (isset($icon))
-                                <label><img src="{{ asset('storage/images/' . $icon) }}" width="80"
-                                        height="80"></label>
+                                <label class="input"><img src="{{ asset('storage/images/' . $icon) }}"></label>
                             @else
-                                <label>登録なし</label>
+                                <label class="input">登録なし</label>
                             @endif
                             <input type="hidden" name="icon" value="{{ $icon }}">
                         </td>
@@ -60,15 +61,23 @@
                 </table>
 
                 <div class="mt-2 border px-4 py-3 rounded relative bg-warning-subtle">
-                    <label style="display: flex; justify-content: center;">上記の内容で登録します。よろしいですか？</label>
+                    <label class="worning">上記の内容で登録します。よろしいですか？</label>
                 </div>
 
                 <div class="button">
-                    <button type="submit" class="btn btn-primary">はい</button>
-                    <a href="{{ route('profile.edit') }}" class="btn btn-danger">いいえ</a>
-                    <a href="{{ route('allTweetGet') }}" class="btn btn-secondary">ＴＯＰ画面へ戻る</a>
+                    <button type="submit" class="btn btn-primary">
+                        <span class="material-symbols-outlined"> check_circle </span>
+                        <span class="btn-name">はい</span>
+                    </button>
+                    <a href="{{ route('profile.edit') }}" class="btn btn-danger">
+                        <span class="material-symbols-outlined"> cancel </span>
+                        <span class="btn-name">いいえ</span>
+                    </a>
+                    <a href="{{ route('allTweetGet') }}" class="btn btn-secondary">
+                        <span class="material-symbols-outlined navbar-follow-btn"> undo </span>
+                        <span class="btn-name">ＴＯＰ画面へ戻る</span>
+                    </a>
                 </div>
-
             </form>
         </div>
     </div>
